dc61142eaeb008b13c7c1f619511edb5eddd23e1
Paul Colomiets <paul@colomiets.name>
2017-08-03 02:18:00 +0300

    Add command-line utility to render templates

--- src/bin/trimmer.rs
+++ src/bin/trimmer.rs
@@ -2,12 +2,13 @@ extern crate trimmer;
 extern crate argparse;
 #[cfg(feature="serde")] extern crate serde_json;
 
-use std::io::Read;
+use std::io::{Read, Write, stdout, stderr};
 use std::fs::File;
 use std::path::{Path};
 use std::process::exit;
 
 use trimmer::Parser;
+#[cfg(feature="serde")] use serde_json::Value;
 
 
 fn main() {
@@ -43,9 +44,119 @@ fn main() {
         }
         ap.parse_args_or_exit();
     }
+    let parser = Parser::new();
+    // TODO(tailhook) use eprintln when we can upgrade to rust 1.19
     if let Some(out_file) = output {
-        unimplemented!();
+        if templates.len() != 1 {
+            writeln!(&mut stderr(),
+                "Exactly one template might be specified when in \
+                 render mode (with `-o`/`--render-to_file`)").ok();
+            exit(1);
+        }
+        let path = Path::new(&templates[0]);
+
+        #[cfg(feature="serde")]
+        let parsed_jsons = {
+            let mut v = Vec::new();
+            for val in &json_vars {
+                match serde_json::from_str::<Value>(val) {
+                    Ok(Value::Object(map)) => v.push(map),
+                    Ok(val) => {
+                        writeln!(&mut stderr(),
+                            "Json must contain object, not {:?}", val).ok();
+                        exit(1);
+                    }
+                    Err(e) => {
+                        writeln!(&mut stderr(),
+                            "Can't parse json: {}", e).ok();
+                        exit(1);
+                    }
+                }
+            }
+            v
+        };
+
+
+        let mut parsed_vars = Vec::new();
+        for pair in vars {
+            let mut piter = pair.splitn(2, '=');
+            match (piter.next(), piter.next()) {
+                (Some(""), _) | (None, _) => {
+                    writeln!(&mut stderr(),
+                        "Var name must not be empty in {:?}",
+                        pair).ok();
+                    exit(1);
+                }
+                (_, None) => {
+                    writeln!(&mut stderr(),
+                        "Var {:?} must contain equals sign",
+                        pair).ok();
+                    exit(1);
+                }
+                (Some(x), Some(y)) => {
+                    parsed_vars.push((x.to_string(), y.to_string()));
+                }
+            }
+        }
+
+        let mut buf = String::with_capacity(1024);
+        let read = File::open(path)
+            .and_then(|mut f| f.read_to_string(&mut buf));
+        match read {
+            Ok(_) => {},
+            Err(e) => {
+                writeln!(&mut stderr(), "Error reading {:?}: {}",
+                    path, e).ok();
+                exit(1);
+            }
+        }
+        let template = match parser.parse(&buf) {
+            Ok(tpl) => tpl,
+            Err(e) => {
+                writeln!(&mut stderr(),
+                    "Error parsing {:?}: {}", path, e).ok();
+                exit(2);
+            }
+        };
+
+        let mut context = trimmer::Context::new();
+        #[cfg(feature="serde")]
+        for map in &parsed_jsons {
+            for (k, v) in map {
+                context.set(k, v);
+            }
+        }
+        for &(ref key, ref value) in &parsed_vars {
+            context.set(key, value);
+        }
+
+        let buf = match template.render(&context) {
+            Ok(value) => value,
+            Err(e) => {
+                writeln!(&mut stderr(), "Error rendering {:?}: {}",
+                    path, e).ok();
+                exit(3);
+            }
+        };
+
+        let res = if out_file == "-" {
+            stdout().write_all(buf.as_bytes())
+        } else {
+            File::create(&out_file)
+                .and_then(|mut f| f.write_all(&buf.as_bytes()))
+        };
+
+        match res {
+            Ok(()) => {}
+            Err(e) => {
+                writeln!(&mut stderr(), "Error writing output {:?}: {}",
+                    out_file, e).ok();
+                exit(3);
+            }
+        }
+
     } else {
+
         #[cfg(not(feature="serde"))]
         let has_vars = vars.len() > 0;
         #[cfg(feature="serde")]
@@ -56,7 +167,6 @@ fn main() {
             exit(1);
         }
 
-        let parser = Parser::new();
         let mut buf = String::with_capacity(4096);
         let mut code = 0;
 
@@ -80,7 +190,7 @@ fn main() {
                 Ok(_) => {}
                 Err(e) => {
                     println!("Error parsing {:?}: {}", path, e);
-                    code = 1;
+                    code = 2;
                 }
             }
         }
